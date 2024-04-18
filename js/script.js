3cb775fc10f65350afc2493f1ce0c7d2e34f3bdd
Alex-N40 <alex_n40@proton.me>
2024-04-17 22:59:05 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Change the background color of the footer to #d7ddf3
+var footer = document.querySelector("footer");
+//console.log(footer.outerHTML);
+footer.style.backgroundColor = "#d7ddf3";
+
+// Change the text of the h2 with the class of .tagline to whatever you like
+var tagline = document.querySelector(".tagline");
+//console.log(tagline.outerHTML);
+tagline.innerText = "best presents for your loved ones!";
+//console.log(tagline.innerText);
+
+// Change line height & font size of the address element
+var address = document.querySelector("address");
+//console.log(address.outerHTML);
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+//console.log(address.innerHTML);
+
+// Emphaize the words "every day" in the contact section header
+var available = document.querySelector("h3");
+//console.log(available.outerHTML);
+available.innerHTML = "We're here for you <em>every day</em> of the week.";
+//console.log(available.innerHTML);
+
+
+
+
+
+
